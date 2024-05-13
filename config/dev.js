5b22e1a22876fdcd4c147b1d6cfed4cd7067bd1a
Amit <fridamit@gmail.com>
2024-05-13 09:34:27 +0300

    Connect to mongoAtlas

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+export default  {
+    dbURL: 'mongodb+srv://kolkoreli:TsFrEFaTIbWmJp3G@expensetrackercluster.da8ixqu.mongodb.net/',
+    dbName : 'expenseDB'
+  }
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+export default {
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://kolkoreli:TsFrEFaTIbWmJp3G@expensetrackercluster.da8ixqu.mongodb.net/',
+    dbName : process.env.DB_NAME || 'expenseDB'
+  }
